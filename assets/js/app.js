0ca3956f1e8a184b4e643be7bb1211ad2345fd2c
Alejandro Betancourth <luisbetancourth43@gmail.com>
2024-03-11 05:00:53 -0500

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,37 @@
 //Variables
 const url = 'http://127.0.0.1:5500/User.json';
 const content = document.querySelector('.content');
+const formulario = document.querySelector('#form');
 
 //Eventos
-
+formulario.addEventListener('change', (e) => {
+    ajustarGrid(e.target.value)
+})
 
 
 //Funciones
-cargarJson()
-async function cargarJson() {
+async function cargarJson(datos) {
     const response = await fetch(url)
     const result = await response.json()
-    insertarHTML(result);
+    insertarHTML(result, datos);
 }
 
 let contador = 0;
-function insertarHTML(users) {
-    users.forEach(user => {
+function insertarHTML(users, datos) {
+    const random = users.sort((a,b) => Math.random() - 0.5);
+    const num = +datos + 1;
+    const usuarios = random.slice(0, +datos)
+    console.log(usuarios)
+    usuarios.forEach(user => {
         const {id, nickname, nombre, valor} = user;
         const cards = document.createElement('div');
+        cards.classList.add('col');
         cards.innerHTML += `
-        <div class="col">
             <div class="card" id="${id}" style="width: 18rem;">
                 <div class="card-body">
-                    ?
+                    <img src="./assets/img/${id}.jpg" alt="fruta">
                 </div>
             </div>
-        </div>
         `
         content.appendChild(cards);
         
@@ -36,5 +41,10 @@ function insertarHTML(users) {
 
 function leerNombre(e) {
     console.log(e.target)
-    contador >= 0 && contador < 2 ? (console.log('clickea otro más '), contador += 1) : (console.log('ya no se puede clickear más'))
+    contador >= 0 && contador < 1 ? (console.log('clickea otro más '), contador += 1) : (console.log('ya no se puede clickear más'))
+}
+
+function ajustarGrid(columnas) {
+    content.style.gridTemplateColumns = `repeat(${columnas}, 1fr)`;
+    cargarJson(columnas);
 }
